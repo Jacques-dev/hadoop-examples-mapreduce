20338aac30fe71fc7b5ee8d28edb31d856ddfc58
Jacques <jacques.tellier@hotmail.com>
2021-11-11 14:11:00 +0100

    fin tp

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,6 +1,14 @@
 package com.opstty;
 
 import com.opstty.job.WordCount;
+import com.opstty.job.DistinctDistricts;
+import com.opstty.job.TreeSpecies;
+import com.opstty.job.TreeSpeciesCount;
+import com.opstty.job.MaxHeightSpecies;
+import com.opstty.job.TreesSortedByHeight;
+import com.opstty.job.OldestTreeDistrictSort;
+import com.opstty.job.OldestTreeDistrictReduce;
+import com.opstty.job.MaxTreesDistrict;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class AppDriver {
@@ -11,6 +19,22 @@ public class AppDriver {
         try {
             programDriver.addClass("wordcount", WordCount.class,
                     "A map/reduce program that counts the words in the input files.");
+            programDriver.addClass("distinctDistricts", DistinctDistricts.class,
+                    "A map/reduce program that returns the distinct districts with trees in a predefined CSV formatting.");
+            programDriver.addClass("treeSpecies", TreeSpecies.class,
+                    "A map/reduce program that returns the distinct tree species in the Remarkable Trees of Paris dataset.");
+            programDriver.addClass("treeSpeciesCount", TreeSpeciesCount.class,
+                    "A map/reduce program that returns the distinct tree species (and the number of trees for each one) in the Remarkable Trees of Paris dataset.");
+            programDriver.addClass("maxHeightSpecies", MaxHeightSpecies.class,
+                    "A map/reduce program that returns the highest height of trees per species in the Remarkable Trees of Paris dataset.");
+            programDriver.addClass("treesSortedByHeight", TreesSortedByHeight.class,
+                    "A map/reduce program that returns all the trees in the Remarkable Trees of Paris dataset, sorted by height.");
+            programDriver.addClass("oldestTreeDistrictSort", OldestTreeDistrictSort.class,
+                    "A map/reduce program that returns the district(s) with the oldest tree(s) in the Remarkable Trees of Paris dataset, using a sort.");
+            programDriver.addClass("oldestTreeDistrictReduce", OldestTreeDistrictReduce.class,
+                    "A map/reduce program that returns the district(s) with the oldest tree(s) in the Remarkable Trees of Paris dataset, checking through all the data.");
+            programDriver.addClass("maxTreesDistrict", MaxTreesDistrict.class,
+                    "A map/reduce program that returns the district(s) with the most trees in the Remarkable Trees of Paris dataset, checking through all the data, using the Reducer's cleanup.");
 
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
